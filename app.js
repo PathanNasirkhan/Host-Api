2e229c922f932e738dd04b9eb241d6b27a37542b
PathanNasirkhan <118352895+PathanNasirkhan@users.noreply.github.com>
2023-03-15 13:27:01 +0530

    add cors

--- app.js
+++ app.js
@@ -7,24 +7,6 @@ const products_routes = require("./routes/product");
 const cors = require("cors");
 
 
-
-
-// Set up middleware to enable CORS
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  next();
-});
-
-// Set up your routes and other middleware here
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Server is running on port 3000');
-});
-
-
-
 app.use(cors());
 
 
