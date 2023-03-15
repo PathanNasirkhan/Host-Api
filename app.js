9fd91781dbddddea55ed58559398ca2a716d04a7
PathanNasirkhan <118352895+PathanNasirkhan@users.noreply.github.com>
2023-03-15 11:36:19 +0530

    middleware chnge

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const products_routes = require("./routes/product");
 
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:3000');
   next();
 });
 
